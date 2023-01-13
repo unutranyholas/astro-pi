61f129d51614add3af97f1d61f3e3ecf19d000be
Ihar Trafimovich <unutranyholas@gmail.com>
2023-01-14 00:22:49 +0100

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:18-alpine
+WORKDIR /app
+COPY . .
+RUN npm install --production
+CMD npm run build && node /app/dist/server/entry.mjs
+EXPOSE 3000
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
+    server: {
+        port: 3000,
+        host:true,
+        strictPort: true,
+    }
+});
