8a6cb6b0fc4a9395651eaecd5b87ab90cfb1accd
Ihar Trafimovich <unutranyholas@gmail.com>
2023-01-14 01:57:30 +0100

    Fix ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import node from "@astrojs/node";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
-    server: {
-        port: 3000,
-        host:true,
-        strictPort: true,
-    }
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true
+  },
+  integrations: [tailwind()]
 });
